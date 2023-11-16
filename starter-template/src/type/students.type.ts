83e5b56179c168beb11205b93035058b6306425a
dangtranchinh1107 <dangtranchinh1107@gmail.com>
2023-11-16 21:44:46 +0700

    react-quert

---
+++ starter-template/src/type/students.type.ts
@@ -0,0 +1,14 @@
+
+
+export interface Student {
+    id: number,
+    first_name: string,
+    last_name: string,
+    email: string,
+    gender: string,
+    country: string,
+    avatar: string,
+    btc_address: string,
+}
+
+export type Students = Pick<Student, 'id' | 'email' | 'last_name' | 'avatar'>[]
